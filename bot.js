d03bfece840cdc913620b0b5b692a2f83c419b7f
Luke Turner <lturner35@student.cscc.edu>
2018-07-18 14:35:30 -0600

    Add files via upload
    Change bot file to do its job

--- bot.js
+++ bot.js
@@ -3,8 +3,13 @@ var config = require('./config.js');
 
 var Twitter = new twit(config);
 
-var messages = ["Hello World! -hh", "poop brother -hh", "Did you know I am a robot? -hh"];
-var messageLocation = 0;
+var response = Math.floor(Math.random() * 13);
+
+var messages = ["No","Nope, Jump isn't out","No jump today my friend.","いいえ","JUMP IS NOT OUT",
+                "I'm sorry. No jump today.","When does jump come out? Not today unfortunately",
+                "negative", "no way","nah","no siree","I wish jump was out today :'(","Fat chance!",
+                "go fish"];
+var messageLocation = response;
 
 var writeTweet = function() {
     Twitter.post('statuses/update', {
@@ -12,9 +17,9 @@ var writeTweet = function() {
     }, function(err, data, response) {
         console.log(data)
     });
-    messageLocation += 1;
+    //messageLocation += 1;
 }
 
 writeTweet()
 
-setInterval(writeTweet, 30000);
+//setInterval(writeTweet, 15000);
